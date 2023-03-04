1a6faa8b73fd3c8b8d0ec42bfca5c751d678f928
Luis C Mogrovejo <luis.c.mogrovejo@gmail.com>
2023-03-04 15:07:31 -0500

    readme

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-module.exports.onRpcRequest = async ({ origin, request }) => {
+const abi = require('../abis/abi.json');
+const Web3 = require('web3'); // Import Web3 library
+const web3 = new Web3(window.ethereum);
+const contractAddress = '0x8638BF6B932764Db8C81ECBAA92f36BcAf369cDc';
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+
+import { getMessage } from './message';
+
+export const onRpcRequest = async ({
+  origin,
+  request,
+}) => {
+  // Make the function async
   switch (request.method) {
+    case 'getPendingInvoices': // Add a new case for the new method
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      }); // Request user accounts
+      const pendingInvoices = await contract.methods
+        .getPayments(accounts[0])
+        .call(); // Call the getPayments method on the contract
+      return pendingInvoices; // Return the result
+    case 'payLatestInvoice':
+      const latestInvoice = await contract.methods.getLatestInvoice().call(); // Call the getLatestInvoice method on the contract
+      const result = await contract.methods
+        .payInvoice(latestInvoice)
+        .send({ from: accounts[0] }); // Call the payInvoice method on the contract, reusing the 'accounts' variable from the previous case
+      return result; // Return the transaction result
     case 'hello':
-      return wallet.request({
+      return snap.request({
         method: 'snap_confirm',
         params: [
           {
-            prompt: `Hello, ${origin}!`,
+            prompt: getMessage(origin),
             description:
               'This custom confirmation is just for display purposes.',
             textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+              'Edit the source code to make your snap do what you want.',
           },
         ],
       });
